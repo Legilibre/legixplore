07f3a8585e1aef60b8a023d5a055667a6c5b1970
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-12-12 13:48:23 +0100

    fix(next): fix now.sh config (tmp)

--- packages/legi-ui/next.config.js
+++ packages/legi-ui/next.config.js
@@ -1,2 +1,22 @@
-const withCSS = require("@zeit/next-css");
-module.exports = withCSS();
+let PHASE_PRODUCTION_SERVER = "PHASE_PRODUCTION_SERVER";
+
+try {
+  let { PHASE_PRODUCTION_SERVER } = require("next-server/constants");
+} catch (e) {
+  // pass
+}
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    return {
+      /* production only config */
+      // https://github.com/zeit/next.js/#disabling-file-system-routing
+      useFileSystemPublicRoutes: false
+    };
+  }
+  const withCSS = require("@zeit/next-css");
+  return withCSS({
+    // https://github.com/zeit/next.js/#disabling-file-system-routing
+    useFileSystemPublicRoutes: false
+  });
+};
