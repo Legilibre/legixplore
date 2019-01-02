3e45f5633ddef57adf57ae8c8416df98e3f66201
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-01-02 04:24:59 +0100

    now tests

--- packages/legi-ui/next.config.js
+++ packages/legi-ui/next.config.js
@@ -1,10 +1,13 @@
+const webpack = require("webpack");
 const withCSS = require("@zeit/next-css");
-
-const { PHASE_PRODUCTION_SERVER } = require("next/constants");
 
 module.exports = (phase, { defaultConfig }) => {
   return withCSS({
     // https://github.com/zeit/next.js/#disabling-file-system-routing
-    useFileSystemPublicRoutes: false
+    useFileSystemPublicRoutes: false,
+    webpack: config => {
+      config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+      return config;
+    }
   });
 };
