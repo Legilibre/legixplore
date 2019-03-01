89c4e4a11e32aa3a73d8a79ae7100423af092c32
Adrien Di Pasquale <adrien@dipasquale.fr>
2019-03-01 18:20:26 +0100

    allow passing DB_URL environment variable

--- packages/legi.js/examples/legi.js
+++ packages/legi.js/examples/legi.js
@@ -1,28 +1,3 @@
 const Legi = require("../src/Legi");
 
-// module.exports = new Legi({
-//   client: "pg",
-//   connection: {
-//     host: "vps.revolunet.com",
-//     port: 5444,
-//     user: "legi",
-//     password: "legi",
-//     database: "legi"
-//   }
-// });
-
-module.exports = new Legi({
-  client: "pg",
-  version: "9.6",
-  connection: {
-    host: "127.0.0.1",
-    port: 5444,
-    user: "user",
-    password: "pass",
-    database: "kali"
-  },
-  pool: {
-    min: 0,
-    max: 5
-  }
-});
+module.exports = new Legi();
